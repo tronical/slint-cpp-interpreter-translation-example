d6d393d7d73511c70c90bdbe3bb974bb92e30461
Simon Hausmann <simon.hausmann@slint.dev>
2024-02-23 14:59:08 +0100

    Change template app to use the C++ interpreter

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,36 @@
-#include "appwindow.h"
+#include <slint-interpreter.h>
 
-int main(int argc, char **argv)
-{
-    auto ui = AppWindow::create();
+int main(int argc, char **argv) {
+  slint::interpreter::ComponentCompiler compiler;
+  auto definition = compiler.build_from_path(SOURCE_DIR "/ui/appwindow.slint");
 
-    ui->on_request_increase_value([&]{
-        ui->set_counter(ui->get_counter() + 1);
-    });
+  for (auto diagnostic : compiler.diagnostics()) {
+    std::cerr << (diagnostic.level ==
+                          slint::interpreter::DiagnosticLevel::Warning
+                      ? "warning: "
+                      : "error: ")
+              << diagnostic.message << std::endl;
+    std::cerr << "location: " << diagnostic.source_file;
+    if (diagnostic.line > 0)
+      std::cerr << ":" << diagnostic.line;
+    if (diagnostic.column > 0)
+      std::cerr << ":" << diagnostic.column;
+    std::cerr << std::endl;
+  }
 
-    ui->run();
-    return 0;
+  if (!definition) {
+    std::cerr << "compilation failure!" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  auto ui = definition->create();
+
+  ui->set_callback("request-increase-value", [&](auto) {
+    ui->set_property("counter",
+                     ui->get_property("counter")->to_number().value() + 1);
+    return slint::interpreter::Value();
+  });
+
+  ui->run();
+  return 0;
 }
